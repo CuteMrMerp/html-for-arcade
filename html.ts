7346a9047c1b6ff1f5180d5503581fa53bdbb486
CuteMrMerp <ian.a.covelli@gmail.com>
2022-08-16 11:15:26 -0400

    Update pxt.json, main.blocks, html.ts

---
+++ html.ts
@@ -0,0 +1,25 @@
+namespace HtmlBlocks {
+    //% blocK="Make Button with label $l as $target, with $spr as mouse. On click:"
+    //% handlerStatement
+    export function uibutton(spr: Sprite, target: Sprite, l: String, handler: () => void) {
+        let button = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+        handler();
+    }
+}
